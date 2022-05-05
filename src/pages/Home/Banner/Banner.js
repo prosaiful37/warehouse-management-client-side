d5a4a08d0dddd1e1786c0f4e0cf2c23b132eaa1a
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-05-06 00:38:38 +0600

    banner section done

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { Button, Carousel } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import banner1 from "../../../images/banner/03_slider-image.jpg";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div>
+      <Container className="slider">
+        <Carousel>
+          <Carousel.Item>
+            <Row className="d-flex align-items-center">
+              <Col xs={6} md={6}>
+                <img
+                  className="d-block w-100 img-fluid"
+                  src={banner1}
+                  alt="First slide"
+                />
+              </Col>
+              <Col xs={6} md={6}>
+                <div className="slider-details">
+                  <h3>Best Perfume</h3>
+                  <p>
+                    pens with a note of black pepper to bring in some spice, but
+                    additional top notes include lemon and bergamot.{" "}
+                  </p>
+                  <Button variant="outline-danger">Shop Now</Button>{" "}
+                </div>
+
+                <Carousel.Caption className="slider-car"></Carousel.Caption>
+              </Col>
+            </Row>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Row className="d-flex align-items-center">
+              <Col xs={6} md={6}>
+                <img
+                  className="d-block w-100 img-fluid"
+                  src={banner1}
+                  alt="First slide"
+                />
+              </Col>
+              <Col xs={6} md={6}>
+                <div className="slider-details">
+                  <h3>Best Perfume</h3>
+                  <p>
+                    pens with a note of black pepper to bring in some spice, but
+                    additional top notes include lemon and bergamot.{" "}
+                  </p>
+                  <Button variant="outline-danger">Shop Now</Button>{" "}
+                </div>
+
+                <Carousel.Caption className="slider-car"></Carousel.Caption>
+              </Col>
+            </Row>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Row className="d-flex align-items-center">
+              <Col xs={6} md={6}>
+                <img
+                  className="d-block w-100 img-fluid"
+                  src={banner1}
+                  alt="First slide"
+                />
+              </Col>
+              <Col xs={6} md={6}>
+                <div className="slider-details">
+                  <h3>Best Perfume</h3>
+                  <p>
+                    pens with a note of black pepper to bring in some spice, but
+                    additional top notes include lemon and bergamot.{" "}
+                  </p>
+                  <Button variant="outline-danger">Shop Now</Button>{" "}
+                </div>
+
+                {/* <Carousel.Caption className="bg-dark"></Carousel.Caption> */}
+              </Col>
+            </Row>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
